a5610d87f142d2d61ec7514485fe8c8c96c98f44
Emerson-feral <emerson_feral@hotmail.com>
2022-07-22 14:38:58 -0300

    Metodos para serem desenvolvidos

---
+++ src/main/java/armazem/Armazem.java
@@ -0,0 +1,32 @@
+package armazem;
+
+import Ingredientes.Ingrediente;
+
+import java.util.TreeMap;
+
+public class Armazem {
+    private TreeMap<Ingrediente,Integer> estoque;
+
+    public void cadastrarIngredienteEmEstoque(Ingrediente ingrediente){
+        //TODO
+    }
+
+    public void descadastrarIngredienteEmEstoque(Ingrediente ingrediente){
+        //TODO
+    }
+
+    public void adicionarQuantidadeDoIngredienteEmEstoque(Ingrediente ingrediente, Integer quantidade){
+        //TODO
+    }
+
+    public void reduzirQuantidadeDoIngredienteEmEstoque(Ingrediente ingrediente, Integer quantidade){
+        //TODO
+    }
+
+    public Integer consultarQuantidadeDoIngredienteEmEstoque(Ingrediente ingrediente){
+        //TODO
+        return null;
+    }
+
+
+}
