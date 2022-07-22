d1e921747ff8472c8cdad18dd145d42371b525e1
Emerson-feral <emerson_feral@hotmail.com>
2022-07-22 14:39:32 -0300

    Testes do armazem

---
+++ src/test/java/armazem/ArmazemTest.java
@@ -0,0 +1,17 @@
+package armazem;
+
+import Ingredientes.Ingrediente;
+import org.junit.jupiter.api.Test;
+
+import java.util.TreeMap;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class ArmazemTest {
+
+    @Test
+    public void cadastrarIngredienteEmEstoque(Ingrediente ingrediente){
+        //TODO
+    }
+
+}
